b93ab720bb42f1fec2ea16aeed5fbe3011324264
abhishek-2110 <sharmabishek762@gmail.com>
2024-11-22 13:23:37 +1300

    initialising springboot directory

---
+++ QuizTournamentApp/src/main/java/com/example/QuizTournamentApp/QuizTournamentAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.QuizTournamentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizTournamentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizTournamentAppApplication.class, args);
+	}
+
+}
