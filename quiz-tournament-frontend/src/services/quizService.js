c19f43ea4fe0ab10611fd94589a8c050e43a84a2
abhishek-2110 <sharmabishek762@gmail.com>
2024-12-03 17:06:47 +1300

    services

---
+++ quiz-tournament-frontend/src/services/quizService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+export const fetchAllQuizzes = async () => {
+  const response = await axios.get("http://localhost:8080/api/quizzes/all");
+  return response.data;
+};
+
+export const likeQuiz = async (quizId) => {
+  await axios.post(`http://localhost:8080/api/quizzes/${quizId}/like`);
+};
+
+export const unlikeQuiz = async (quizId) => {
+  await axios.post(`http://localhost:8080/api/quizzes/${quizId}/unlike`);
+};
+
+export const fetchQuizQuestions = async (quizId) => {
+  const response = await axios.get(
+    `http://localhost:8080/api/quizzes/${quizId}/play`
+  );
+  return response.data;
+};
